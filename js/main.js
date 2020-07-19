fab1f79c312556dc30ea8769e375330d5c064f07
Chamty <ak.cha07.mt@gmail.com>
2020-07-19 13:01:01 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
       this.newItemTitle = ''; //入力欄を初期値に戻す
       this.saveTodo(); //ブラウザに保存
     },
-    deleteTodo(index) {
+    doneTodo(index) {
       let doneTodo = this.items[index].title;
       this.items.splice(index, 1);
       this.dones.push({
@@ -29,6 +29,24 @@
         isChecked: true
       });
       this.saveTodo(); //ブラウザに保存
+    },
+    returnTodo(index) {
+      let returnTodo = this.dones[index].title;
+      this.dones.splice(index, 1);
+      this.items.push({
+        title: returnTodo,
+        isChecked: false
+      });
+    },
+    deleteTodo(index) {
+      if (items) {
+      let deleteTodo = this.items[index].title;
+      this.items.slice(index, 1);
+      }
+      if (dones) {
+      let deleteTodo = this.dones[index].title;
+      this.dones.slice(index, 1);
+      }
     },
     saveTodo() {
       localStorage.setItem('items', JSON.stringify(this.items));
